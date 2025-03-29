3dadb4cf277274abe7d9967ad0650500a8953d56
Santiago Hernandez <trollearvpn156@gmail.com>
2025-03-28 22:15:44 -0500

    Se arreglaron problemas con las FK en EntidadesNucleo.cs

---
+++ ut_presentacion/Repositorios/EnviosPrueba.cs
@@ -0,0 +1,54 @@
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ut_presentacion.Nucleo;
+namespace ut_presentacion.Repositorios
+{
+    [TestClass]
+    public class EnviosPrueba
+    {
+        private readonly IConexion? iConexion;
+        private List<Envios>? lista;
+        private Envios? entidad;
+        public EnviosPrueba()
+        {
+            iConexion = new ConexionEF3.Conexion();
+            iConexion.StringConexion = Configuracion.ObtenerValor("StringConexion");
+        }
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            //Assert.AreEqual(true, Borrar());
+        }
+        public bool Listar()
+        {
+            this.lista = this.iConexion!.Envios!.ToList();
+            return lista.Count > 0;
+        }
+        public bool Guardar()
+        {
+            this.entidad = EntidadesNucleo.Envios()!;
+            this.iConexion!.Envios!.Add(this.entidad);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Modificar()
+        {
+            this.entidad!.Transportadora = "Envios Juanchito";
+            var entry = this.iConexion!.Entry<Envios>(this.entidad);
+            entry.State = EntityState.Modified;
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Borrar()
+        {
+            this.iConexion!.Envios!.Remove(this.entidad!);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+    }
+}
