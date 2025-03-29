b2eb8c30b3b0c11f659be55338a10e77e1889464
Santiago Hernandez <trollearvpn156@gmail.com>
2025-03-28 22:48:54 -0500

    Se realizaron las unit tests para peliculas

---
+++ ut_presentacion/Nucleo/EntidadesNucleo.cs
@@ -0,0 +1,93 @@
+﻿using lib_dominio.Entidades;
+namespace ut_presentacion.Nucleo;
+
+public class EntidadesNucleo
+{
+    
+    public static Almacenes? Almacenes()
+    {
+        var entidad = new Almacenes();
+        entidad.Ubicacion_bodega = "AVENIDA COLOMBIA #398 MEDELLIN ANT" ;
+        entidad.Capacidad_bodega = 666.00m;
+        return entidad;
+    }
+    
+    public static Miembros? Miembros()
+    {
+        var entidad = new Miembros();
+        entidad.Nombre = "Santiago Hernandez";
+        entidad.Fecha_registro = new DateTime(2004, 9, 28, 12, 36, 0);
+        entidad.Nivel = "Oro";
+        entidad.Puntos = 6800;
+        return entidad;
+    }
+
+    public static Empleados? Empleados()
+    {
+        var entidad = new Empleados();
+        entidad.Nombre_empleado = "Juan Pérez";
+        entidad.Cargo_empleado = "Desarrollador";
+        entidad.Sueldo = 5000.00m;
+        entidad.Fecha_contratacion = new DateTime(2020, 1, 15);
+        return entidad;
+    }
+
+    public static Envios? Envios()
+    {
+        var entidad = new Envios();
+        entidad.Estado = "En tránsito";
+        entidad.Direccion = "Calle Falsa 123";
+        entidad.Transportadora = "Transportes XYZ";
+        entidad.empleados = 2;
+        return entidad;
+    }
+
+    public static Reservas? Reservas()
+    {
+        var entidad = new Reservas();
+            entidad.Fecha_Reserva = DateTime.Now;
+            entidad.Estado = "Confirmada";
+            entidad.MiembroId = 3;
+            entidad.PeliculaId = 2;
+            entidad.ConsolaId = 5;
+            entidad.EmpleadoId = 4;
+        return entidad;
+    }
+
+    public static Peliculas? Peliculas()
+    {
+        var entidad = new Peliculas();
+        entidad.Nombre_pelicula = "La Gran Aventura";
+        entidad.Genero_Pelicula = "Aventura";
+        entidad.Fecha_Estreno = new DateTime(2022, 5, 20);
+        entidad.Estado_pelicula = true;
+        return entidad;
+    }
+
+    public static Consolas? Consolas()
+    {
+        var entidad = new Consolas();
+        entidad.Tipo_consola = "Videojuegos";
+        entidad.Marca_consola = "Sony";
+        entidad.Estado_consola = 1;
+        entidad.almacen = 7;
+        return entidad;
+    }
+
+    public static Snacks? Snacks()
+    {
+        var entidad = new Snacks();
+        entidad.Nombre = "Chips";
+        entidad.Precio = 1.50m;
+        entidad.Stock = 100;
+        return entidad;
+    }
+
+    public static Reservas_Snacks? Reservas_Snacks()
+    {
+        var entidad = new Reservas_Snacks();
+        entidad.SnackId = 4;
+        entidad.Reserva = 3;
+        return entidad;
+    }
+}
---
+++ ut_presentacion/Repositorios/ReservasPruebas.cs
@@ -0,0 +1,54 @@
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ut_presentacion.Nucleo;
+namespace ut_presentacion.Repositorios
+{
+    [TestClass]
+    public class ReservasPruebas
+    {
+        private readonly IConexion? iConexion;
+        private List<Reservas>? lista;
+        private Reservas? entidad;
+        public ReservasPruebas()
+        {
+            iConexion = new ConexionEF3.Conexion();
+            iConexion.StringConexion = Configuracion.ObtenerValor("StringConexion");
+        }
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            Assert.AreEqual(true, Borrar());
+        }
+        public bool Listar()
+        {
+            this.lista = this.iConexion!.Reservas!.ToList();
+            return lista.Count > 0;
+        }
+        public bool Guardar()
+        {
+            this.entidad = EntidadesNucleo.Reservas()!;
+            this.iConexion!.Reservas!.Add(this.entidad);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Modificar()
+        {
+            this.entidad!.Estado = "Probando unit test";
+            var entry = this.iConexion!.Entry<Reservas>(this.entidad);
+            entry.State = EntityState.Modified;
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Borrar()
+        {
+            this.iConexion!.Reservas!.Remove(this.entidad!);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+    }
+}
