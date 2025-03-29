1ce4ded9a70dbfc996a52420f442c576d34cc53e
Santiago Hernandez <trollearvpn156@gmail.com>
2025-03-28 23:37:58 -0500

    Pruebas unitarias terminadas con exito

---
+++ ut_presentacion/Repositorios/Reservas_SnacksPruebas.cs
@@ -0,0 +1,54 @@
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ut_presentacion.Nucleo;
+namespace ut_presentacion.Repositorios
+{
+    [TestClass]
+    public class ReservasSnacksPruebas
+    {
+        private readonly IConexion? iConexion;
+        private List<Reservas_Snacks>? lista;
+        private Reservas_Snacks? entidad;
+        public ReservasSnacksPruebas()
+        {
+            iConexion = new ConexionEF3.Conexion();
+            iConexion.StringConexion = Configuracion.ObtenerValor("StringConexion");
+        }
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            //Assert.AreEqual(true, Borrar());
+        }
+        public bool Listar()
+        {
+            this.lista = this.iConexion!.Reservas_Snacks!.ToList();
+            return lista.Count > 0;
+        }
+        public bool Guardar()
+        {
+            this.entidad = EntidadesNucleo.Reservas_Snacks()!;
+            this.iConexion!.Reservas_Snacks!.Add(this.entidad);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Modificar()
+        {
+            this.entidad!.Id_Reservas_Snacks = entidad!.Id_Reservas_Snacks;
+            var entry = this.iConexion!.Entry<Reservas_Snacks>(this.entidad);
+            entry.State = EntityState.Modified;
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Borrar()
+        {
+            this.iConexion!.Reservas_Snacks!.Remove(this.entidad!);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+    }
+}
--- ut_presentacion/obj/Debug/net9.0/ut_presentacion.AssemblyInfo.cs
+++ ut_presentacion/obj/Debug/net9.0/ut_presentacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1390d293635447c908612f4297657ae49c46e16e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d224361767f96feb5099647ed7c24d33c261f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
