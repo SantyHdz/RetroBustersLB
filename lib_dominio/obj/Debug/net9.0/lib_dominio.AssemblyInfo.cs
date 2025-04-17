ee6d16211aa25eb08a7ab8a3081e6ff7e27d57d1
Santiago Hernandez <trollearvpn156@gmail.com>
2025-04-17 11:40:45 -0500

    Interfaz Arreglada

--- lib_dominio/obj/Debug/net9.0/lib_dominio.AssemblyInfo.cs
+++ lib_dominio/obj/Debug/net9.0/lib_dominio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef3542c141e6d158e8e3321a776569f22f57382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37566174a651a85af8574a1e29d4558730916267")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
