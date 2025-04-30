27519d9f215b67e9c36c9360f5b74ec1f9403ec2
Sos1498 <moralesmateo277@gmail.com>
2025-04-29 20:32:21 -0500

    Agregacion de SnacksAplicacion con su respectiva interfaz

---
+++ lib_aplicaciones/Interfaces/ISnacksAplicacion.cs
@@ -0,0 +1,12 @@
+﻿using lib_dominio.Entidades;
+namespace lib_aplicaciones.Interfaces;
+
+public interface ISnacksAplicacion
+{
+    void Configurar(string StringConexion);
+    List<Snacks> Listar();
+    Snacks? Guardar(Snacks? entidad);
+    Snacks? Modificar(Snacks? entidad);
+    Snacks? Borrar(Snacks? entidad);
+    List<Snacks> PorNombre(Snacks? entidad);
+}
--- lib_aplicaciones/obj/Debug/net9.0/lib_aplicaciones.AssemblyInfo.cs
+++ lib_aplicaciones/obj/Debug/net9.0/lib_aplicaciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_aplicaciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5231ccdbd8c227408aab43347ea72443f2c32949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c35acddb5be4f04063fd65974ad39d0e3e838f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_aplicaciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_aplicaciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
