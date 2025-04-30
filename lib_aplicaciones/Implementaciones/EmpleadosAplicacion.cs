5231ccdbd8c227408aab43347ea72443f2c32949
Sos1498 <moralesmateo277@gmail.com>
2025-04-29 20:17:58 -0500

    Agregacion de EmpleadosAplicacion con su respectiva interfaz

---
+++ lib_aplicaciones/Implementaciones/EmpleadosAplicacion.cs
@@ -0,0 +1,61 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones;
+
+public class EmpleadosAplicacion : IEmpleadosAplicacion
+{
+    private IConexion? IConexion = null;
+
+    public EmpleadosAplicacion(IConexion iConexion)
+    {
+        this.IConexion = iConexion;
+    }
+
+    public void Configurar(string StringConexion)
+    {
+        this.IConexion!.StringConexion = StringConexion;
+    }
+
+    public Empleados? Guardar(Empleados? entidad)
+    {
+        if (entidad == null) throw new Exception("lbFaltaInformacion");
+        if (entidad.Id != 0) throw new Exception("lbYaSeGuardo");
+        this.IConexion!.Empleados!.Add(entidad);
+        this.IConexion.SaveChanges();
+        return entidad;
+    }
+
+    public Empleados? Modificar(Empleados? entidad)
+    {
+        if (entidad == null) throw new Exception("lbFaltaInformacion");
+        if (entidad.Id == 0) throw new Exception("lbNoSeGuardo");
+        var entry = this.IConexion!.Entry<Empleados>(entidad);
+        entry.State = EntityState.Modified;
+        this.IConexion.SaveChanges();
+        return entidad;
+    }
+
+    public Empleados? Borrar(Empleados? entidad)
+    {
+        if (entidad == null) throw new Exception("lbFaltaInformacion");
+        if (entidad.Id == 0) throw new Exception("lbNoSeGuardo");
+        this.IConexion!.Empleados!.Remove(entidad);
+        this.IConexion.SaveChanges();
+        return entidad;
+    }
+
+    public List<Empleados> Listar()
+    {
+        return this.IConexion!.Empleados!.Take(20).ToList();
+    }
+
+    public List<Empleados> PorCargo(Empleados? entidad)
+    {
+        return this.IConexion!.Empleados!
+            .Where(x => x.Cargo!.Contains(entidad!.Cargo!))
+            .ToList();
+    }
+}
---
+++ lib_aplicaciones/Interfaces/IEmpleadosAplicacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_dominio.Entidades;
+namespace lib_aplicaciones.Interfaces;
+
+public interface IEmpleadosAplicacion
+{
+    void Configurar(string StringConexion);
+    List<Empleados> PorCargo(Empleados? entidad);
+    List<Empleados> Listar();
+    Empleados? Guardar(Empleados? entidad);
+    Empleados? Modificar(Empleados? entidad);
+    Empleados? Borrar(Empleados? entidad);
+
+}
