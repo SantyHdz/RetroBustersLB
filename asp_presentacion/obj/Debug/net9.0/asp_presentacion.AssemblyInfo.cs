20dcc3d9d31b3b972d70e597eb0d728def124092
Sos1498 <moralesmateo277@gmail.com>
2025-06-03 00:21:31 -0500

    Se agrega la generacion de reportes para Empleados

--- asp_presentacion/obj/Debug/net9.0/asp_presentacion.AssemblyInfo.cs
+++ asp_presentacion/obj/Debug/net9.0/asp_presentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp_presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c2397d2791f406e121ff61c2f453467157a9b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725cde30673166428b35204f415a233a92e31061")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp_presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp_presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
